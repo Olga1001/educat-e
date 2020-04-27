bb345234f70b559b3d5dc41509427562d1f20d28
Olha <ololoevaevaeva@gmail.com>
2020-04-27 12:19:11 +0200

    commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,14 +1,10 @@
 $(document).ready(function () {
     //stopPropagation
-    $(".dropdown-menu").click(function (e) {
+    $(".dropdown-menu, .popup .form-box, .btn-add-lesson, .edit").click(function (e) {
         e.stopPropagation();
     });
 
 //HEADER
-    $(".navbar .btn").click(function (e) {
-        e.preventDefault();
-        $(this).toggleClass('active').siblings().removeClass('active');
-    });
     //burger
     $(".navbar-toggler").click(function (e) {
         e.preventDefault();
@@ -57,30 +53,50 @@ $(document).ready(function () {
             },
         ]
     });
-    $("#files").on("change", function (evt) {
-        let files = evt.target.files; // FileList object
+    //search
+    $(".btn-search.bg-white").click(function (e) {
+        e.preventDefault();
+        $("body, html").animate({scrollTop: $(".nav.nav-tabs").offset().top - 100},{duration:1E3});
+    });
+
+//PROFILE
+
+    //load
+    $("#files").on("change", function (e) {
+        let file =  this.files[0];
         document.getElementById('loader-avatar').innerHTML = "";
-        for (let i = 0, f; f = files[i]; i++) {
-            // Only process image files.
-            if (!f.type.match('image.*')) {
-                alert("Только изображения....");
-            }
-            let reader = new FileReader();
-            // Closure to capture the file information.
-            reader.onload = (function(theFile) {
-                return function(e) {
-                    // Render thumbnail.
-                    let label = document.createElement('label');
-                    label.innerHTML = ['<img class="loader-avatar_img" src="', e.target.result,
-                        '" title="', escape(theFile.name), '"/>'].join('');
-                    document.getElementById('loader-avatar').insertBefore(label, null);
-                    $(".delete").addClass('active');
-                };
-            })(f);
-            // Read in the image file as a data URL.
-            reader.readAsDataURL(f);
+
+        let label = document.createElement('label');
+        label.innerHTML = ['<img class="loader-avatar_img" src="img/', file.name,
+            '" title="', escape(file.name), '"/>'].join('');
+        document.getElementById('loader-avatar').insertBefore(label, null);
+
+        $(".delete").addClass('active');
+    });
+
+//POPUPS
+
+    function setPopupPosition() {
+        if ( $(".popup-contain .form-box").height() >= $(window).height()) {
+            $(".popup-contain").addClass('height');
+        } else {
+            $(".popup-contain").removeClass('height');
         }
-        return false;
+    }
+    setPopupPosition();
+    $(window).resize( setPopupPosition );
+
+    $(".close, .popup-contain, .popup").click(function () {
+        $(".popups").removeClass('active');
+        $(".popup").removeClass('active');
+    });
+
+//MODULE LESSONS
+
+    $(".list__module").click(function () {
+        $(this).closest(".list-item").toggleClass('active').siblings().removeClass('active');
+        $(this).closest(".list-item").find(".list__lessons").slideToggle(300).closest(".list-item").siblings().find(".list__lessons").slideUp(300);
     });
 });
+
 
